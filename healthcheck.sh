6699c5d745984cec49fbf72d7d274305c7840190
Alexander Chertovsky <alexzaets.dev@gmail.com>
2025-04-08 18:47:40 +0300

    more verbocity on why failed

--- healthcheck.sh
+++ healthcheck.sh
@@ -5,4 +5,5 @@ bin/console -e test cache:clear || exit 1
 vendor/bin/phpunit --stop-on-error --stop-on-failure || exit 1
 bin/console -edev
 vendor/bin/phptools || exit 1
-php -d xdebug.mode=coverage vendor/bin/phpunit --stop-on-error --stop-on-failure --coverage-text | grep 'Classes: 100.00%' || exit 1
+echo "Running: php -d xdebug.mode=coverage vendor/bin/phpunit --stop-on-error --stop-on-failure --coverage-text"
+php -d xdebug.mode=coverage vendor/bin/phpunit --stop-on-error --stop-on-failure --coverage-text | grep 'Classes: 100.00%' || { echo "Command failed: General phpunit coverage check"; exit 1; }
